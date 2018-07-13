2bb49f8025dfc54a77f0fc44012df1510121f798
Simon Bothén <caresilabs@gmail.com>
2018-07-13 12:48:41 +0200

    MusicManager improvements

--- FigtherFluff/Assets/Scripts/MusicManager.cs
+++ FigtherFluff/Assets/Scripts/MusicManager.cs
@@ -4,6 +4,10 @@ public class MusicManager : MonoBehaviour
 {
     public static MusicManager Instance;
 
+    private AudioSource MusicSource;
+
+    private AudioSource FxSource;
+
     void Awake()
     {
         if (Instance != null && Instance != this)
@@ -14,6 +18,22 @@ public class MusicManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        this.MusicSource = GetComponents<AudioSource>()[0];
+        this.FxSource = GetComponents<AudioSource>()[1];
+    }
+
+    public void PlaySound(AudioClip sound, float volume)
+    {
+        if (sound != null)
+        {
+            FxSource.clip = sound;
+            FxSource.volume = volume;
+            FxSource.Play();
+        }
     }
 
 }
